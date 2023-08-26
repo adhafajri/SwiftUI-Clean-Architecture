e1577aba695db4a2294bbf46919101bb607a8f31
Muhammad Adha Fajri Jonison <madhafajrij@gmail.com>
2023-08-26 19:08:21 +0700

    Pokemon app without clean architecture

--- Pokemon Clean Architecture/ContentView.swift
+++ Pokemon Clean Architecture/ContentView.swift
@@ -6,83 +6,86 @@
 //
 
 import SwiftUI
-import CoreData
+
+struct Pokemon: Identifiable, Decodable {
+    let id: Int
+    let name: String
+    var imageUrl: URL? {
+        return URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/\(id).png")
+    }
+}
+
+struct PokemonList: Decodable {
+    let results: [PokemonResult]
+}
+
+struct PokemonResult: Decodable {
+    let name: String
+    let url: String
+}
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    @State private var pokemonList: [Pokemon] = []
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        List {
+            ForEach(pokemonList) { pokemon in
+                PokemonView(pokemon: pokemon)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
         }
-    }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
+        .task {
             do {
-                try viewContext.save()
+                pokemonList = try await getPokemonList()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                print("error")
             }
         }
     }
+    
+    func getPokemonList() async throws -> [Pokemon] {
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?limit=50") else {
+            throw URLError(.badURL)
+        }
+        
+        let request = URLRequest(url: url)
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+            throw URLError(.badServerResponse)
+        }
+        
+        var pokemons = [Pokemon]()
+        
+        let pokemonList = try JSONDecoder().decode(PokemonList.self, from: data)
+        
+        for (index, result) in pokemonList.results.enumerated() {
+            let pokemon = Pokemon(id: index + 1, name: result.name)
+            pokemons.append(pokemon)
+        }
+        
+        return pokemons
+    }
+}
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+struct PokemonView: View {
+    var pokemon: Pokemon
+    
+    var body: some View {
+        HStack {
+            AsyncImage(url: pokemon.imageUrl) { image in
+                image
+                    .image?
+                    .resizable()
             }
+            .scaledToFit()
+            .frame(width: 100, height: 100)
+            Text(pokemon.name)
         }
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
