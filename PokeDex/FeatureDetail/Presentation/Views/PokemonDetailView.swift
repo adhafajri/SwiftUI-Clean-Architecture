e380e4cbe1d9213b59b977bd324f57ea250f57db
Muhammad Adha Fajri Jonison <madhafajrij@gmail.com>
2023-09-03 21:18:19 +0700

    Detail view MVVM

---
+++ PokeDex/FeatureDetail/Presentation/Views/PokemonDetailView.swift
@@ -0,0 +1,45 @@
+//
+//  PokemonDetailView.swift
+//  PokeDex
+//
+//  Created by Muhammad Adha Fajri Jonison on 28/08/23.
+//
+
+import SwiftUI
+
+struct PokemonDetailView: View {
+    private let viewModel: PokemonDetailViewModel = PokemonDetailViewModel()
+    var id: Int
+    
+    var body: some View {
+        VStack {
+            if viewModel.pokemonDetail == nil {
+                Text("Loading...")
+            } else {
+                AsyncImage(url: viewModel.pokemonDetail?.pokemon.imageURL) { image in
+                    image.image?.resizable()
+                }
+                .scaledToFit()
+                
+                Text(viewModel.pokemonDetail?.pokemon.name ?? "")
+                    .font(.title)
+                
+                HStack {
+                    Text("Height: \(viewModel.pokemonDetail?.height ?? 0)")
+                        .font(.subheadline)
+                    Text("Weight: \(viewModel.pokemonDetail?.weight ?? 0)")
+                        .font(.subheadline)
+                }
+            }
+        }
+        .task {
+            viewModel.loadDetail(id: id)
+        }
+    }
+}
+
+struct PokemonDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonDetailView(id: 1)
+    }
+}
