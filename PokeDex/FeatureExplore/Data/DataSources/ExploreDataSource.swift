d912b8b35c51037b759486ec794702b5f5ebd371
Muhammad Adha Fajri Jonison <madhafajrij@gmail.com>
2023-09-05 22:09:42 +0700

    minor fix

--- PokeDex/FeatureExplore/Data/DataSources/ExploreDataSource.swift
+++ PokeDex/FeatureExplore/Data/DataSources/ExploreDataSource.swift
@@ -15,12 +15,4 @@ class ExploreDataSource {
         
         return try await NetworkUtils.shared.fetch(from: url)
     }
-    
-    func fetchPokemonDetail(id: Int) async throws -> PokemonDetailReponseModel {
-        guard let url: URL = Constants.APIEndpoint.getPokemonDetails(id: id).url else {
-            throw URLError(.badURL)
-        }
-        
-        return try await NetworkUtils.shared.fetch(from: url)
-    }
 }
